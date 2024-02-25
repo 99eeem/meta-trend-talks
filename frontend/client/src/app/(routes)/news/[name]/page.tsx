5d0f88521ac429699dc0668033b89a32efe588b7
99eeem <99eeem@gmail.com>
2024-02-25 21:12:10 +0800

    [MTT] add back and forth function

--- frontend/client/src/app/(routes)/news/[name]/page.tsx
+++ frontend/client/src/app/(routes)/news/[name]/page.tsx
@@ -1,91 +1,93 @@
-import { StringDecoder } from "string_decoder";
-import { getNewsListByCategory } from "../../../_features/news/api/getNewsListByCategory"
-import { getNewsList } from "../../../_features/news/api/getNewsList"
-import { getCategoryList, getCategory} from "../../../_features/news/api/getNewsCategory"
-import styles from './page.module.scss'; // SCSSファイルを読み込む 
-import Title from '../../../_components/ui/title/title'
-import CategoryList from "../../../_components/ui/categoryList/categoryList"
-import NewsArticle from "../../../_components/page/article/NewsArticle";
-import Pagination from "../../../_components/ui/pagination/pagenation"
-import ArticleList from  "../../../_components/page/news/ArticleList"
-import type { Metadata, ResolvingMetadata } from 'next'
-import {NewsType} from "../../../_const/interface/news"
-  
-  export async function generateMetadata({ params }: {params : {name: string}}, parent: ResolvingMetadata): Promise<Metadata> {
-        let news: NewsType[] = []
-      let title: string | undefined = undefined
-      let description: string | undefined = undefined
-        if (params.name === 'all'){
-            title = '全てのニュース'
-            description = `ニュースを全てまとめた情報サイト。Web3、NFT、Defiなど、幅広いトピックを網羅。いち早く最新の出来事をキャッチしよう。`
-        }else {
-            const category = await getCategory(params.name)
-            news = await getNewsListByCategory(category[0].id)
-            title = category[0].name
-            description = `ニュースを${category[0].name}の項目でまとめた情報サイト。${category[0].name}のニュースを網羅。いち早く最新の出来事をキャッチしよう。`
-        }
-    return {
-        title,
-        description,
-        openGraph: {
-        title,
-        description,
-        url: `https://${process.env.MTT_DOMAIN}`,
-        siteName: 'metaTrendTalks.com',
-        images: [
-            {
-            url: `https://${process.env.MTT_DOMAIN}/images/logo.png`,
-            width: 800,
-            height: 600,
-            },
-            {
-            url: `https://${process.env.MTT_DOMAIN}/images/logo.png`,
-            width: 1800,
-            height: 1600,
-            },
-        ],
-        locale: 'ja_JP',
-        type: 'website',
-        },
-    }
-  }
-  
+import { StringDecoder } from 'string_decoder';
+import { getNewsListByCategory } from '../../../_features/news/api/getNewsListByCategory';
+import { getNewsList } from '../../../_features/news/api/getNewsList';
+import { getCategoryList, getCategory } from '../../../_features/news/api/getNewsCategory';
+import styles from './page.module.scss'; // SCSSファイルを読み込む
+import Title from '../../../_components/ui/title/title';
+import CategoryList from '../../../_components/ui/categoryList/categoryList';
+import NewsArticle from '../../../_components/page/article/NewsArticle';
+import Pagination from '../../../_components/ui/pagination/pagenation';
+import ArticleList from '../../../_components/page/news/ArticleList';
+import type { Metadata, ResolvingMetadata } from 'next';
+import { NewsType } from '../../../_const/interface/news';
 
-export default async function Page({params}: {params : {name: string}}){
-    try {
-        let news: NewsType[] = []
-        let title: string | undefined = undefined
-        if (params.name === 'all'){
-            news = await getNewsList()
-            title = '全てのニュース'
-        }else {
-            const category = await getCategory(params.name)
-            if (!category.length) return 
-            news = await getNewsListByCategory(category[0].id)
-            title = category[0].name
-        }
-    return (
-        <div className={styles.newsByCategory}>
-            <div className={styles.leftContainer}>
-                <Title title={title}/>
-                <ArticleList news={news}/>
-            </div>
-            <div className={styles.rightContainer}>
-                <CategoryList/>
-            </div>
-        </div>
-      );
-    } catch (error) {
-        console.log('build error', error)
-        return error
-    }
+export async function generateMetadata(
+  { params }: { params: { name: string } },
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  let news: NewsType[] = [];
+  let title: string | undefined = undefined;
+  let description: string | undefined = undefined;
+  if (params.name === 'all') {
+    title = '全てのニュース';
+    description = `ニュースを全てまとめた情報サイト。Web3、NFT、Defiなど、幅広いトピックを網羅。いち早く最新の出来事をキャッチしよう。`;
+  } else {
+    const category = await getCategory(params.name);
+    news = await getNewsListByCategory(category[0].id);
+    title = category[0].name;
+    description = `ニュースを${category[0].name}の項目でまとめた情報サイト。${category[0].name}のニュースを網羅。いち早く最新の出来事をキャッチしよう。`;
+  }
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      url: `https://${process.env.MTT_DOMAIN}`,
+      siteName: 'metaTrendTalks.com',
+      images: [
+        {
+          url: `https://${process.env.MTT_DOMAIN}/images/logo.png`,
+          width: 800,
+          height: 600,
+        },
+        {
+          url: `https://${process.env.MTT_DOMAIN}/images/logo.png`,
+          width: 1800,
+          height: 1600,
+        },
+      ],
+      locale: 'ja_JP',
+      type: 'website',
+    },
+  };
 }
 
-export async function generateStaticParams(){
-    const res = await getCategoryList()
-    const params = res.map(item => {
-        return { name: item.link.toString() };
-      })
-      params.push({name: 'all'})
-    return params
+export default async function Page({ params }: { params: { name: string } }) {
+  try {
+    let news: NewsType[] = [];
+    let title: string | undefined = undefined;
+    if (params.name === 'all') {
+      news = await getNewsList();
+      title = '全てのニュース';
+    } else {
+      const category = await getCategory(params.name);
+      if (!category.length) return;
+      news = await getNewsListByCategory(category[0].id);
+      title = category[0].name;
+    }
+    return (
+      <div className={styles.newsByCategory}>
+        <div className={styles.leftContainer}>
+          <Title title={title} />
+          <ArticleList news={news} />
+        </div>
+        <div className={styles.rightContainer}>
+          <CategoryList />
+        </div>
+      </div>
+    );
+  } catch (error) {
+    console.log('build error', error);
+    return error;
+  }
+}
+
+export async function generateStaticParams() {
+  const res = await getCategoryList();
+  const params = res.map((item) => {
+    return { name: item.link.toString() };
+  });
+  params.push({ name: 'all' });
+  return params;
 }
---
+++ frontend/client/src/app/_components/page/article/BackAndForthNews.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './BackAndForthNews.module.scss';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { NewsWithRelatedNewsType } from '../../../_const/interface/news';
+import Link from 'next/link';
+import next from 'next';
+
+const BackAndForthNews = ({ item }: { item: any }) => {
+  console.log('item', item);
+  return (
+    <>
+      {Object.keys(item).length && (
+        <div className={styles.backAndForthNews}>
+          <Link href={`/article/${item.back.id}`} className={styles.backNews}>
+            <FaChevronLeft color={'#CCCCCC'} />
+            <div className={styles.news}>
+              <div>
+                <span>前の記事</span>
+                <h5>{item.back.title}</h5>
+              </div>
+            </div>
+          </Link>
+          <Link href={`/article/${item.next.id}`} className={styles.nextNews}>
+            <div className={styles.news}>
+              <div>
+                <span>次の記事</span>
+                <h5>{item.next.title}</h5>
+              </div>
+            </div>
+            <FaChevronRight color={'#CCCCCC'} />
+          </Link>
+        </div>
+      )}
+    </>
+  );
+};
+export default BackAndForthNews;
